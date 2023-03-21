6995c6fbd05bbeb8c1ca57202bf8c9cef41b41c8
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-21 03:21:37 -0400

    click our story test

---
+++ tests/click_our_story_page.js
@@ -0,0 +1,13 @@
+const { test, expect } = require('@playwright/test')
+
+test('Click Our Story page', async ({ page }) => {
+  // Navigate to the homepage
+  await page.goto('https://mdellis73.github.io/mywebclass-simulation/')
+
+  // Click on the "Our Story" link
+  await page.click('text=Our Story')
+
+  // Check that the page title is correct
+  const pageTitle = await page.title()
+  expect(pageTitle).toBe('MyWebClass.org | Our Story')
+})
