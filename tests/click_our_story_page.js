1f5a0051c33a46e9ded82c23ce338dfe2e64e56c
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-21 10:33:41 -0400

    click test

--- tests/click_our_story_page.js
+++ tests/click_our_story_page.js
@@ -1,6 +1,10 @@
 const { test, expect } = require('@playwright/test')
+
 test('Click Our Story page', async ({ page }) => {
   // Navigate to the homepage
-  await page.goto('https://mdellis73.github.io/mywebclass-simulation/story.html')
+  await page.goto('https://mdellis73.github.io/mywebclass-simulation')
 
+  // Click the Agree button
+  await page.click('#agreeButton')
+  await page.goto('https://mdellis73.github.io/mywebclass-simulation/story.html')
 })
