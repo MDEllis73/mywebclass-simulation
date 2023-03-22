8229e63a34f42f530429b210512469dc439212b2
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-22 04:04:49 -0400

    Added Mobile Test

---
+++ tests/pil_mobile_privacy_page.spec.js
@@ -0,0 +1,10 @@
+import { test, expect, devices } from '@playwright/test'
+test.use({
+  ...devices['iPhone X']
+})
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+  await page.getByRole('button', { name: 'Agree', exact: true }).click()
+  await page.click('a:has-text("Privacy Policy")')
+})
