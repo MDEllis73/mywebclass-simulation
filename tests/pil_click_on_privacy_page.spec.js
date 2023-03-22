df8f34c7ac060c5b9ac2bf71183c8e075f652edb
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-21 23:55:55 -0400

    fix prviacy test

--- tests/pil_click_on_privacy_page.spec.js
+++ tests/pil_click_on_privacy_page.spec.js
@@ -4,4 +4,4 @@ test('Click on Privacy', async ({ page }) => {
   await page.goto('http://localhost:3000/')
   await page.getByRole('button', { name: 'Agree', exact: true }).click()
   await page.getByRole('link', { name: 'Privacy Policy' }).click()
-});
+})
