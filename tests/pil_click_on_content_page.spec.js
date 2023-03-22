0629ca98f305331c3a9e7f69fc7e7f36e300fca1
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-21 23:45:31 -0400

    ddDD

--- tests/pil_click_on_content_page.spec.js
+++ tests/pil_click_on_content_page.spec.js
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('Click on Content', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.getByRole('button', { name: 'Agree', exact: true }).click();
-  await page.getByRole('link', { name: 'Content Template' }).click();
-});
+  await page.goto('http://localhost:3000/')
+  await page.getByRole('button', { name: 'Agree', exact: true }).click()
+  await page.getByRole('link', { name: 'Content Template' }).click()
+})
--- tests/pil_click_on_privacy_page.js
+++ tests/pil_click_on_privacy_page.js
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('Click on Privacy', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.getByRole('button', { name: 'Agree', exact: true }).click();
-  await page.getByRole('link', { name: 'Privacy Policy' }).click();
+  await page.goto('http://localhost:3000/')
+  await page.getByRole('button', { name: 'Agree', exact: true }).click()
+  await page.getByRole('link', { name: 'Privacy Policy' }).click()
 });
