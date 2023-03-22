25cebac3e2323a83c541b5b4a83371b27b3ce845
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-21 23:35:10 -0400

    dd

--- tests/pil_click_on_privacy_page.js
+++ tests/pil_click_on_privacy_page.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Click on Content', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByRole('button', { name: 'Agree', exact: true }).click();
-  await page.getByRole('link', { name: 'Content Template' }).click();
+  await page.getByRole('link', { name: 'Privacy Policy' }).click();
 });
--- tests/pil_click_our_story_page.spec.js
+++ tests/pil_click_our_story_page.spec.js
@@ -1,6 +1,6 @@
-const { test, expect } = require('@playwright/test')
+import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Click on Story Page', async ({ page }) => {
   await page.goto('http://localhost:3000/')
   await page.getByRole('button', { name: 'Agree', exact: true }).click()
   await page.getByRole('link', { name: 'Our Story' }).click()
