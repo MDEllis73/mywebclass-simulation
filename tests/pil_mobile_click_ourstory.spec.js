968262cda062a8fd94d37029ad209c6e8a585b16
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-22 13:33:13 -0400

    Galaxy s9+

--- tests/pil_mobile_click_ourstory.spec.js
+++ tests/pil_mobile_click_ourstory.spec.js
@@ -1,12 +1,11 @@
 import { test, devices } from '@playwright/test'
 
 test.use({
-  ...devices['iPhone 11']
+  ...devices['Galaxy S9+']
 })
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:3000/')
   await page.getByRole('button', { name: 'Agree', exact: true }).click()
   await page.getByRole('link', { name: 'Our Story' }).click()
-  await page.getByText('Learning: We believe that learning is a lifelong process and that everyone has s').click()
 })
