98ee47b84a420d6443b5e34723dccdb19df92669
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-21 21:21:47 -0400

    click our story

--- tests/mywebclass_title.spec.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-const { test, expect } = require('@playwright/test')
-
-test('Should have MyWebClass.org title', async ({ page }) => {
-  // Expect a title "to contain" a substring.
-  await page.goto('https://mdellis73.github.io/mywebclass-simulation/')
-  await expect(page).toHaveTitle('MyWebClass.org')
-})
