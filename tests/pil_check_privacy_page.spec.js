fc088c7b399cb8891621532c060687442dc58419
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-21 23:26:41 -0400

    d

--- tests/pil_check_privacy_page.spec.js
+++ tests/pil_check_privacy_page.spec.js
@@ -1,7 +1,7 @@
-const { test, expect } = require('@playwright/test')
+import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
-  await page.getByRole('button', { name: 'Agree', exact: true }).click()
-  await page.getByRole('link', { name: 'Privacy Policy' }).click()
-})
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Agree', exact: true }).click();
+  await page.getByRole('link', { name: 'Content Template' }).click();
+});
