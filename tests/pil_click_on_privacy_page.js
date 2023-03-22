21d180121519120a8d65b39f5bf51b62597bbce6
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-21 23:40:35 -0400

    dd

--- tests/pil_click_on_privacy_page.js
+++ tests/pil_click_on_privacy_page.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Click on Content', async ({ page }) => {
+test('Click on Privacy', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByRole('button', { name: 'Agree', exact: true }).click();
   await page.getByRole('link', { name: 'Privacy Policy' }).click();
