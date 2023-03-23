248c908e604887ce77b5eb17bf9c2222e3936d0d
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-22 23:50:49 -0400

    hey changed the URL

--- tests/pil_click_on_privacy_page.spec.js
+++ tests/pil_click_on_privacy_page.spec.js
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test'
 
 test('Click on Privacy', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
+  await page.goto('https://mdellis73.github.io/mywebclass-simulation/')
   await page.getByRole('button', { name: 'Agree', exact: true }).click()
   await page.click('a:has-text("Privacy Policy")')
 })
--- tests/pil_mobile_actions.spec.js
+++ tests/pil_mobile_actions.spec.js
@@ -5,7 +5,7 @@ test.use({
 })
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
+  await page.goto('https://mdellis73.github.io/mywebclass-simulation/')
   await page.getByRole('button', { name: 'Agree', exact: true }).click()
   await page.getByText('Monthly digest of what\'s new and exciting from us.').click()
   await page.getByRole('link', { name: 'Our Story' }).click()
