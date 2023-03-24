fde8a9fc8d703fa538864a73b63e4eff82a0fef5
Asingh027 <as696@gmail.com>
2023-03-24 15:31:55 -0400

    Added new tests and removed ones that weren't working

--- tests/avneet_server_responds_under_five_hundred_ms.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Server response time should be under 500ms', async ({ page }) => {
-  const startTime = Date.now();
-  await page.goto('http://mdellis73.github.io/mywebclass-simulation');
-  const endTime = Date.now();
-  const responseTime = endTime - startTime;
-  expect(responseTime).toBeLessThanOrEqual(500);
-});
