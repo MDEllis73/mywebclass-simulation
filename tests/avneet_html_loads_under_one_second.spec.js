1b772e4cfc454f46b6c03f2a3baf9842e091d159
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-24 00:38:16 -0400

    testing at the moment

--- tests/avneet_html_loads_under_one_second.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Initial HTML should load in under 1 second', async ({ page }) => {
-  const startTime = Date.now();
-  await page.goto('http://mdellis73.github.io/mywebclass-simulation');
-  const endTime = Date.now();
-  const loadTime = endTime - startTime;
-  expect(loadTime).toBeLessThanOrEqual(1000);
-});
--- tests/pil_click_our_story_page.spec.js
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/test'
-
-test('Click on Story Page', async ({ page }) => {
-  await page.goto('https://mdellis73.github.io/mywebclass-simulation/')
-  await page.getByRole('button', { name: 'Agree', exact: true }).click()
-  await page.getByRole('link', { name: 'Our Story' }).click()
-})
