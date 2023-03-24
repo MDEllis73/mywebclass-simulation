609c34d5111699f1d4193eec36d4f0013b5ab015
M Darrin Ellis <123265440+MDEllis73@users.noreply.github.com>
2023-03-24 01:23:25 -0400

    Correct page address.

--- t_e_s_t_s/mike_mobile_email_submit.spec.js
+++ t_e_s_t_s/mike_mobile_email_submit.spec.js
@@ -5,7 +5,7 @@ test.use({
 })
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
+  await page.goto('https://mdellis73.github.io/mywebclass-simulation/')
   await page.getByRole('button', { name: 'Agree', exact: true }).click()
   await page.getByPlaceholder('Email address').click()
   await page.getByPlaceholder('Email address').fill('mde7@njit.edu')
