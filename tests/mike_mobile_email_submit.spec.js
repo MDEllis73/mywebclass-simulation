e265b296cb310e24b914db5f3448dcf0be465e84
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-25 23:28:13 -0400

    dfdfd

--- tests/mike_mobile_email_submit.spec.js
+++ tests/mike_mobile_email_submit.spec.js
@@ -6,6 +6,7 @@ test.use({
 
 test('test', async ({ page }) => {
   await page.goto('https://mdellis73.github.io/mywebclass-simulation/')
+  await page.goto('http://localhost:3000/')
   await page.getByRole('button', { name: 'Agree', exact: true }).click()
   await page.getByPlaceholder('Email address').click()
   await page.getByPlaceholder('Email address').fill('mde7@njit.edu')
--- tests/pil_five_seconds.spec.js
+++ tests/pil_five_seconds.spec.js
@@ -7,6 +7,15 @@ test.use({
 test('test', async ({ page }) => {
   await page.goto('https://mdellis73.github.io/mywebclass-simulation/')
   await page.getByRole('button', { name: 'Agree', exact: true }).click()
+  // Wait for 1 second
+  await page.waitForTimeout(1000)
   await page.getByRole('button', { name: 'Toggle navigation' }).click()
   await page.getByRole('link', { name: 'Content Template' }).click()
+  // Wait for 1 second
+  await page.waitForTimeout(1000)
+  await page.getByRole('link', { name: 'MyWebClass.org' }).click()
+  // Wait for 1 second
+  await page.waitForTimeout(1000)
+  await page.goto('https://mdellis73.github.io/mywebclass-simulation/')
+  await page.getByRole('link', { name: 'Privacy Policy' }).click()
 })
