4734eacf96aeb11ac272583b15616e21c69f7034
Asingh027 <as696@gmail.com>
2023-03-25 21:27:01 -0400

    Added playwright test that checks that our website meets GDPR standards using third party website.

--- tests/mike_desktop_email_wrong_submit.spec.js
+++ tests/mike_desktop_email_wrong_submit.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test } from '@playwright/test'
 
 test('test', async ({ page }) => {
   await page.goto('https://mdellis73.github.io/mywebclass-simulation/')
