201d66a2298c61b13d4ec90cc70cc94a7f8aff1f
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-03-26 00:16:20 -0400

    dfdf

--- tests/pil_mobile_actions_privacy.spec.js
+++ tests/pil_mobile_actions_privacy.spec.js
@@ -7,7 +7,7 @@ test.use({
 test('test', async ({ page }) => {
   await page.goto('https://mdellis73.github.io/mywebclass-simulation/')
   await page.getByRole('button', { name: 'Agree', exact: true }).click()
-  await page.click('a:has-text("Privacy Policy")')
+  await page.goto('https://mdellis73.github.io/mywebclass-simulation/privacy.html')
   await page.getByRole('heading', { name: 'How We Use Your Info' }).click()
   await page.getByRole('heading', { name: 'Changes to this Privacy Policy' }).click()
   await page.getByRole('link', { name: 'MyWebClass.org' }).click()
--- tests/pil_mobile_click_our_story.spec.js
+++ tests/pil_mobile_click_our_story.spec.js
@@ -5,7 +5,7 @@ test.use({
 })
 
 test('test', async ({ page }) => {
-  await page.goto('https://mdellis73.github.io/mywebclass-simulation/')
+  await page.goto('https://mdellis73.github.io/mywebclass-simulation/index.html')
   await page.getByRole('button', { name: 'Agree', exact: true }).click()
   await page.getByRole('link', { name: 'Our Story' }).click()
 })
